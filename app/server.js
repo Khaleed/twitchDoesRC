6905cbcb58a8d35d05b359a379b1487d40106ac7
Khalid Omar Ali <khalid.ali@consultant.com>
2015-11-10 14:58:32 -0500

    set up databases

--- app/server.js
+++ app/server.js
@@ -4,9 +4,32 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server); // initialise a new instance of io and pass http server
 var port = process.env.port || 3000;
-var redis = require("redis");
+// var redis = require("redis");
 var config = require('../config/config_local.json');
-var redisClient = redis.createClient(config.redis);
+// var redisClient = redis.createClient(config.redis);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('data.db');
+var fs = require("fs");
+var file = "data.db";
+var exists = fs.existsSync(file);
+
+db.serialize(function() {
+  if (!exists) {
+  	db.run("CREATE TABLE Game_Command_History (command TEXT)");
+  }
+  // put in 
+  var stmt = db.prepare("INSERT INTO Game_Command_History VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Game_Command" + i);
+  }
+  stmt.finalize();
+  // select 
+  db.each("SELECT rowid AS id, info as avocado FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.avocado);
+  });
+});
+
+db.close();
 
 // redisClient.set("test-key", 3000, function () {
 // 	redisClient.get("test-key", function (err, val) {
@@ -15,9 +38,9 @@ var redisClient = redis.createClient(config.redis);
 // });
 
 var irc = require('irc');
-var channelOwner = 'your username';
-var password = 'put your twitch OAuth here';
-var channel = '#channel';
+var channelOwner = 'username';
+var password = 'your oauth';
+var channel = '#user2';
 
 var options = {
 	userName: channelOwner, // mandatory
@@ -30,7 +53,7 @@ var options = {
 	autoConnect: false,
 	// channels: [channel],
 	// secure: false,
-	// selfSigned: false,
+	// selfSigned: falsße,
 	// certExpired: false,
 	// floodProtection: false,
 	// floodProtectionDelay: 1000,
