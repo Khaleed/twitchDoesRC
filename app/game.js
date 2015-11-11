0224088ada4537aff80f7835ae6da65e8b83e51d
Khalid Omar Ali <khalid.ali@consultant.com>
2015-11-11 17:07:12 -0500

    count current votes

--- app/game.js
+++ app/game.js
@@ -1,21 +1,21 @@
 exports.isValidMove = function() {
-
 	var legalMoves = {
 		'k': "UP",
 		'j': "DOWN",
 		'h': "LEFT",
 		'l': "RIGHT"
-	};
+};
 
 	return function(move) {
-		if (legalMoves[move]) {
-			console.log("legal move " + move);
-			return true;
-		} else {
-			console.log("illegal move " + move);
-			return false;
-		}
-	};
+	// 	// if (legalMoves[move]) {
+	return true;
+	// 	// 	console.log("legal move " + move);
+	// 	// 	return true;
+	// 	// } else {
+	// 	// 	console.log("illegal move " + move);
+	// 	// 	return false;
+	// 	// }
+};
 }();
 
 exports.getValidMoves = function(moves) {
@@ -23,8 +23,6 @@ exports.getValidMoves = function(moves) {
 	// moves is an array of messages.
  	return moves.filter(exports.isValidMove);
 };
-
-
 
 // isValidMove('k');
 // isValidMove('q');
