4928274a8ca4a6398b3be06c427065ce5a2c3cf1
Khalid Omar Ali <khalid.ali@consultant.com>
2015-11-17 18:38:09 -0500

    clean up comment

--- app/keyhandler.js
+++ app/keyhandler.js
@@ -4,12 +4,12 @@ let robot = require("robotjs");
 
 module.exports = {
 	sendKey: function (key) {
-		// first argument is the key  to press, can be up down left right or something else
-		// the second argument is either down or up, which means press or release key
+		// first argument is the key  to press, can be up down le
+		// the second is press or release key
 		robot.keyToggle(key, "down");
 		// once the key is pressed, you need to release it whenever  you see fit (usually 150ms)
 		setTimeout(function() {
 			robot.keyToggle(key, "up");
 		}, 150);
 	}
-}
+};
